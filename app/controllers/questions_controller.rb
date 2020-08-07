6e2101efdd2f24d26af8e16da86eaf50ddf84ddf
Craig Horan <craighoran8@gmail.com>
2020-08-06 17:40:43 -0700

    completed stupid coaching challenge

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -9,8 +9,12 @@ class QuestionsController < ApplicationController
     # store question in HTML using an instance variable
     @question = params[:question]
     # store the answer depending on the question
-    if @question == "I am going to work"
-      @answer = "Great!"
+    if @question == 'I am going to work'
+      @answer = 'Great!'
+    elsif @question.end_with?('?')
+      @answer = 'Silly question, get dressed and and go to work!'
+    else
+      @answer = "I don't care, get dressed and go to work!"
     end
   end
 end
